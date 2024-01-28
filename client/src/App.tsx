857ee30cda9db1e8fb60665df75f990a03352074
Marshall Blatz <marshallpblatz@gmail.com>
2024-01-27 18:49:12 -0600

    added in all content and formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Linkedin, Github, Mail } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="flex w-screen h-screen items-center justify-center">
+            <div className="w-[750px] flex flex-col space-y-8">
+                <div className="space-y-3">
+                    <div className="flex space-x-4 items-center">
+                        <h3 className="text-xl">Marshall Blatz</h3>
+                        <a href="https://www.linkedin.com/in/marshall-blatz/" target="_blank">
+                            <Linkedin size={18} />
+                        </a>
+                        <a href="https://github.com/moorshall" target="_blank">
+                            <Github size={18} />
+                        </a>
+                        <a href="mailto:marshallpblatz@gmail.com" target="_blank">
+                            <Mail size={18} />
+                        </a>
+                    </div>
+                    <p className="text-grey">
+                        I’m a software engineer passionate about using technology to solve complex problems and create
+                        friendly user interfaces.
+                    </p>
+                </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <div className="space-y-3">
+                    <p>Projects</p>
+                    <p className="text-grey">
+                        <a
+                            className=" underline items-center text-white"
+                            href="https://cureight.vercel.app"
+                            target="_blank"
+                        >
+                            Curate <i className="ti ti-external-link"></i>
+                        </a>
+                        {"  "}- A useful tool for sharing playlists independent of music service, and a platform for
+                        playlist curators to show off.
+                    </p>
+                    <p className="text-grey">
+                        <a
+                            className=" underline items-center text-white"
+                            href="https://amplo.vercel.app"
+                            target="_blank"
+                        >
+                            Amplo <i className="ti ti-external-link"></i>
+                        </a>
+                        {"  "}- A simple, but effective habit tracking application (I turned the github contribution
+                        chart into a habit tracker).
+                    </p>
+                </div>
+
+                <div className="space-y-3">
+                    <p>Work</p>
+                    <div>
+                        <div className="flex space-x-4">
+                            <a
+                                className=" underline items-center text-white"
+                                href="https://irrc.education.uiowa.edu/"
+                                target="_blank"
+                            >
+                                Iowa Reading Research Center <i className="ti ti-external-link"></i>
+                            </a>
+                            <p>(November 2021 - Present)</p>
+                        </div>
+                        <p className="text-grey">
+                            Software Developer - Building and maintaining software to support researchers of children’s
+                            literacy.
+                        </p>
+                    </div>
+                    <div>
+                        <div className="flex space-x-4">
+                            <a className=" underline items-center text-white" href="https://rsmus.com/" target="_blank">
+                                RSM <i className="ti ti-external-link"></i>
+                            </a>
+                            <p>(Summer 2022)</p>
+                        </div>
+                        <p className="text-grey">
+                            Application Development Intern - A consulting role where I built extensions for Microsoft’s
+                            ERP Platform, Business Central.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
